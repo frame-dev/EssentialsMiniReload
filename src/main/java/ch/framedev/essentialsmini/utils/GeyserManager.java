fe7f70107bb3a3af2b82893b7056e13691c8f10d
framedev <darrylhuber39@gmail.com>
2025-10-01 11:30:21 +0200

    Remove unused GeyserApi import and simplify GeyserManager class

--- src/main/java/ch/framedev/essentialsmini/utils/GeyserManager.java
+++ src/main/java/ch/framedev/essentialsmini/utils/GeyserManager.java
@@ -1,9 +1,5 @@
 package ch.framedev.essentialsmini.utils;
 
-import org.geysermc.geyser.api.GeyserApi;
-
-import java.util.UUID;
-
 public class GeyserManager {
 
     public static boolean isGeyserInstalled() {
@@ -11,18 +7,6 @@ public class GeyserManager {
             Class.forName("org.geysermc.geyser.api.GeyserApi");
             return true;
         } catch (ClassNotFoundException e) {
-            return false;
-        }
-    }
-
-    public static boolean isBedrockPlayer(UUID uuid) {
-        if (!isGeyserInstalled()) {
-            return false;
-        }
-        try {
-            return GeyserApi.api().isBedrockPlayer(uuid);
-        } catch (Exception e) {
-            e.printStackTrace();
             return false;
         }
     }
